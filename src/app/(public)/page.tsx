723e03d9b2bcaa78773ecbb2cad452dce38259ed
Abdul Rehman <ar618211@gmail.com>
2025-04-12 23:51:36 +0500

    feat: purge and biome and basic layout

---
+++ src/app/(public)/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="flex w-full h-full flex-col items-center justify-center gap-4">
+      <h1>Some sort of Calculator</h1>
+      <p>{'<LandingPage />'}</p>
+    </div>
+  );
+}
---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <footer className="text-center p-4 text-sm">
+      <a
+        href="https://abdul-rehman-d.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-blue-500 hover:underline"
+      >
+        Visit Abdul Rehman Daniyal
+      </a>
+      <p>&copy; 2025 All rights reserved.</p>
+    </footer>
+  );
+}
